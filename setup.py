08aae62b17f039044ca3659cfab4a3e50958b03b
Nicodemos Varnava <nicodemosvarnava@gmail.com>
2024-02-09 15:22:17 +0200

    get_products api

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="2050_materials_api_client",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        "requests",  # Add your dependencies here
+    ],
+)
